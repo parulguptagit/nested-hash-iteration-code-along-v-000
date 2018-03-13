bb76b8d005692d71114a41dfeca21fd940fc96b8
Parul Gupta <parulguptasmail@gmail.com>
2018-03-13 19:48:01 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+    contacts.each do |person, details|
+      if person == "Freddy Mercury"
+        details.each do |attribute, value|
+          if attribute == :favorite_ice_cream_flavors 
+            value.delete_if {|ice_cream| ice_cream == "strawberry"}
+          end
+        end
+      end
+    end
 end
