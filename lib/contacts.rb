75ae05880c35e70983ba3ac80e9cbf7ae6eacb89
Parul Gupta <parulguptasmail@gmail.com>
2018-03-13 19:47:37 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
     contacts.each do |person, details|
       if person == "Freddy Mercury"
         details.each do |attribute, value|
-          if attribute == favorite_ice_cream_flavors 
+          if attribute == :favorite_ice_cream_flavors 
             value.delete_if {|ice_cream| ice_cream == "strawberry"}
           end
         end
